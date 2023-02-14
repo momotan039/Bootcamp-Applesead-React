a639d5c957ed0d235d9ad7efa9b2753b66b896d3
momotan <momotaha039@gmail.com>
2023-02-14 13:24:43 +0200

    HideAndSeek

--- Hide And Seek/src/App.jsx
+++ Hide And Seek/src/App.jsx
@@ -6,11 +6,6 @@ import { useEffect } from 'react'
 
 function App() {
   const [isBox1,show_Box1]=useState(true)
-  // const [boxs,show_Box]=useState({show1:true,sohw2:false})
-
-  // useEffect(()=>{
-  //   console.log('from parent',isBox1);
-  // })
 
   const toggleBoxes=()=>{
     show_Box1(!isBox1)
--- Hide And Seek/src/comp.jsx
+++ Hide And Seek/src/comp.jsx
@@ -3,12 +3,6 @@ import { useState } from "react"
 
 function Comp(props){
 
-// let [isShow,setShow]=useState(props.ShowBox)
-
-// useEffect(()=>{
-//     console.log('from child',isShow);
-//   })
-  
 return<div>
 <button onClick={()=>{props.toggleBoxes()}}>{props.ShowBox?'Hide Box':'Show Box'}</button>
 <div className={props.ShowBox?'box':'box hidden'}></div>
