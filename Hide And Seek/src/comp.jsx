e903cfa27572aeb6997df89cfb9e719b730b5ea6
momotan <momotaha039@gmail.com>
2023-02-14 14:04:56 +0200

    Increment and decrement

--- Hide And Seek/src/comp.jsx
+++ Hide And Seek/src/comp.jsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { useState } from "react"
 
 function Comp(props){
-
 return<div>
 <button onClick={()=>{props.toggleBoxes()}}>{props.ShowBox?'Hide Box':'Show Box'}</button>
 <div className={props.ShowBox?'box':'box hidden'}></div>
---
+++ Increment and decrement/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  let [count, setCount] = useState(0)
+  function changeColor(){
+  if(count==0)
+  return ''
+  else if(count>0)
+  return 'green'
+   
+  return 'red'
+}
+  return (
+    <div className="App">
+      <h1 className={changeColor()}>Your number:{count}</h1>
+        <button onClick={()=>setCount(++count)}>Plus</button>
+        <button onClick={()=>setCount(--count)}>Minus</button>
+    </div>
+  )
+}
+
+export default App
