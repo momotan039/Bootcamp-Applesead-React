91e977f72ade0ee9cf2815479d8d3bc379f7a934
momotan <momotaha039@gmail.com>
2023-02-20 18:16:54 +0200

    grid spinner

---
+++ spinner/src/Grid Spinner/GridSpinner.jsx
@@ -0,0 +1,16 @@
+import './GridSpinner.css'
+function GridSpinner() {
+    return <div class="lds-grid">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+}
+
+export default GridSpinner
