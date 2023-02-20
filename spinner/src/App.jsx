a4a3a1265c2e27438ec914059534abe64a34632b
MOHAMMED <momotaha039@gmail.com>
2023-02-20 20:26:09 +0200

    finish spinners

--- spinner/src/App.jsx
+++ spinner/src/App.jsx
@@ -1,20 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Spinner from './Spinner/Spinner'
-import GridSpinner from './Grid Spinner/GridSpinner'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Spinner from "./Spinner/Spinner";
+import GridSpinner from "./Grid Spinner/GridSpinner";
+import CubeSpinner from "./Cube Spinner/CubeSpinner";
 
 function App() {
-  const [count, setCount] = useState(0)
-  
-  const spinner=<Spinner/>
-  
-  return (
-    <div className="App">
-      {/* {spinner} */}
-      <GridSpinner/>
-    </div>
-  )
+  const spinners = [<Spinner />, <GridSpinner />,<CubeSpinner/>];
+  const num = Math.floor(Math.random() * spinners.length);
+  const spinner = spinners[num];
+  const [showSpinner, setShowSpinner] = useState(false);
+  const [timer, setTimer] = useState(true);
+
+  // when mount component
+  useEffect(() => {
+    setTimeout(() => {
+      setTimer(false);
+    }, 2000);
+  }, []);
+
+  //when change timer value
+  useEffect(() => {
+    setShowSpinner(!showSpinner);
+  }, [timer]);
+
+  return <div className="App">
+    {showSpinner && spinner}
+    <h1>Please Wait A While</h1>
+    </div>;
 }
 
-export default App
+export default App;
