00eb1ee6d4c5f634930183d633c68d0dfb449987
MOHAMMED <momotaha039@gmail.com>
2023-02-28 13:00:08 +0200

    fetch method by custome hooks

--- Custome Hooks/src/App.jsx
+++ Custome Hooks/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { Articles } from './components/Articles'
+import { Books } from './components/Books'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Articles/>
+      <Books/>
     </div>
   )
 }
--- Custome Hooks/src/components/Books.jsx
+++ Custome Hooks/src/components/Books.jsx
@@ -1,11 +1,14 @@
+import { useFetch } from "./useFetch"
+
 export const Books = () => {
     const urlApi='https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=nNAneMM7JgClj6NfL9hmR8smXQFDp5YR'
     const data=useFetch(urlApi)
+    debugger
     return (
         <div className="books">
         <h1>Books</h1>
             {
-                data&&data.result.books.map((d,i)=>{
+                data&&data.results.books.map((d,i)=>{
                     return <div key={i} className="book">
                     <h3>{d.title}</h3>
                  </div>
